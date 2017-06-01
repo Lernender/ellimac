92e07e1573bab9685536971818e9fb5f45f0f3dc
Lernender <c.peter@woche-pass.ch>
2017-06-01 09:43:40 +0200

    Save 01-06-17

--- ellimac/lib/Ellimac/Controller.php
+++ ellimac/lib/Ellimac/Controller.php
@@ -19,141 +19,141 @@ use Website\Controller\ProjectsController;
 
 class Controller
 {
-//    protected $controller = 'projects';
-//    protected $method = 'index';
-//    protected $params = [];
-//
-//    public function __construct()
-//    {
-//        $this->parseUrl();
-//    }
-//
-//    public function parseUrl()
-//    {
-//        if (isset($_GET['controller'])) {
-//            echo 'Controller: ' . $_GET['controller'];
-//        }
-//
-//        if (isset($_GET['action'])) {
-//            echo 'Action: ' . $_GET['action'];
-//        }
-//    }
+    protected $controller = 'projects';
+    protected $method = 'index';
+    protected $params = [];
 
-//    /**
-//     * @throws \Zend_Controller_Response_Exception
-//     */
-//    public function init()
-//    {
-//        parent::init();
-//        $this->view->setRequest($this->getRequest());
-//        // set content type
-//        if ($this->getResponse()->canSendHeaders()) {
-//            $this->getResponse()->setHeader("Content-Type", "text/html; charset=UTF-8", true);
-//        }
-//    }
+    public function __construct()
+    {
+        $this->parseUrl();
+    }
 
-//    /**
-//     * @throws \Zend_Controller_Response_Exception
-//     */
-//    protected function disableBrowserCache()
-//    {
-//        // set this headers to avoid problems with proxies, ...
-//        if ($this->getResponse()->canSendHeaders()) {
-//            $this->getResponse()->setHeader("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0, max-age=0", true);
-//            $this->getResponse()->setHeader("Cache-Control", "no-transform"); // this is for mod_pagespeed
-//            $this->getResponse()->setHeader("Pragma", "no-cache", true);
-//            $this->getResponse()->setHeader("Expires", "Tue, 01 Jan 1980 00:00:00 GMT", true);
-//        }
-//    }
+    public function parseUrl()
+    {
+        if (isset($_GET['controller'])) {
+            echo 'Controller: ' . $_GET['controller'];
+        }
 
-//    /**
-//     *
-//     */
-//    protected function removeViewRenderer()
-//    {
-//        \Zend_Controller_Action_HelperBroker::removeHelper('viewRenderer');
-//        $this->viewEnabled = false;
-//    }
+        if (isset($_GET['action'])) {
+            echo 'Action: ' . $_GET['action'];
+        }
+    }
 
-//    /**
-//     * @return null|\Zend_Layout
-//     */
-//    protected function layout()
-//    {
-//        return $this->enableLayout();
-//    }
+    /**
+     * @throws \Zend_Controller_Response_Exception
+     */
+    public function init()
+    {
+        parent::init();
+        $this->view->setRequest($this->getRequest());
+        // set content type
+        if ($this->getResponse()->canSendHeaders()) {
+            $this->getResponse()->setHeader("Content-Type", "text/html; charset=UTF-8", true);
+        }
+    }
 
-//    /**
-//     * @return null|\Zend_Layout
-//     * @throws \Zend_Controller_Action_Exception
-//     */
-//    protected function enableLayout()
-//    {
-//        $viewRenderer = \Zend_Controller_Action_HelperBroker::getExistingHelper("viewRenderer");
-//        $viewRenderer->setIsInitialized(false); // reset so that the view get's initialized again, because of error page from other modules
-//        $viewRenderer->initView();
-//        \Zend_Layout::startMvc();
-//        $layout = \Zend_Layout::getMvcInstance();
-//        $layout->enableLayout();
-//        $layout->setViewSuffix(\Pimcore\View::getViewScriptSuffix());
-//        return $layout;
-//    }
+    /**
+     * @throws \Zend_Controller_Response_Exception
+     */
+    protected function disableBrowserCache()
+    {
+        // set this headers to avoid problems with proxies, ...
+        if ($this->getResponse()->canSendHeaders()) {
+            $this->getResponse()->setHeader("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0, max-age=0", true);
+            $this->getResponse()->setHeader("Cache-Control", "no-transform"); // this is for mod_pagespeed
+            $this->getResponse()->setHeader("Pragma", "no-cache", true);
+            $this->getResponse()->setHeader("Expires", "Tue, 01 Jan 1980 00:00:00 GMT", true);
+        }
+    }
 
-//    /**
-//     *
-//     */
-//    protected function disableLayout()
-//    {
-//        $layout = \Zend_Layout::getMvcInstance();
-//        if ($layout) {
-//            $layout->disableLayout();
-//        }
-//    }
+    /**
+     *
+     */
+    protected function removeViewRenderer()
+    {
+        \Zend_Controller_Action_HelperBroker::removeHelper('viewRenderer');
+        $this->viewEnabled = false;
+    }
 
-//    /**
-//     * @param $name
-//     * @return $this
-//     */
-//    protected function setLayout($name)
-//    {
-//        $layout = \Zend_Layout::getMvcInstance();
-//        if ($layout instanceof \Zend_Layout) {
-//            $layout->setLayout($name);
-//        }
-//        return $this;
-//    }
+    /**
+     * @return null|\Zend_Layout
+     */
+    protected function layout()
+    {
+        return $this->enableLayout();
+    }
 
-//    /**
-//     *
-//     */
-//    protected function disableViewAutoRender()
-//    {
-//        $this->_helper->viewRenderer->setNoRender();
-//    }
+    /**
+     * @return null|\Zend_Layout
+     * @throws \Zend_Controller_Action_Exception
+     */
+    protected function enableLayout()
+    {
+        $viewRenderer = \Zend_Controller_Action_HelperBroker::getExistingHelper("viewRenderer");
+        $viewRenderer->setIsInitialized(false); // reset so that the view get's initialized again, because of error page from other modules
+        $viewRenderer->initView();
+        \Zend_Layout::startMvc();
+        $layout = \Zend_Layout::getMvcInstance();
+        $layout->enableLayout();
+        $layout->setViewSuffix(\Pimcore\View::getViewScriptSuffix());
+        return $layout;
+    }
 
-//    /**
-//     * @param $path
-//     * @return bool
-//     */
-//    protected function viewScriptExists($path)
-//    {
-//        $scriptPaths = $this->view->getScriptPaths();
-//        foreach ($scriptPaths as $scriptPath) {
-//            if (is_file($scriptPath . $path)) {
-//                return true;
-//            }
-//        }
-//    }
+    /**
+     *
+     */
+    protected function disableLayout()
+    {
+        $layout = \Zend_Layout::getMvcInstance();
+        if ($layout) {
+            $layout->disableLayout();
+        }
+    }
 
-//    /**
-//     *
-//     */
-//    public function preDispatch()
-//    {
-//        if ($this->hasParam("_segment")) {
-//            $this->_helper->viewRenderer->setResponseSegment($this->getParam("_segment"));
-//        }
-//    }
+    /**
+     * @param $name
+     * @return $this
+     */
+    protected function setLayout($name)
+    {
+        $layout = \Zend_Layout::getMvcInstance();
+        if ($layout instanceof \Zend_Layout) {
+            $layout->setLayout($name);
+        }
+        return $this;
+    }
+
+    /**
+     *
+     */
+    protected function disableViewAutoRender()
+    {
+        $this->_helper->viewRenderer->setNoRender();
+    }
+
+    /**
+     * @param $path
+     * @return bool
+     */
+    protected function viewScriptExists($path)
+    {
+        $scriptPaths = $this->view->getScriptPaths();
+        foreach ($scriptPaths as $scriptPath) {
+            if (is_file($scriptPath . $path)) {
+                return true;
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function preDispatch()
+    {
+        if ($this->hasParam("_segment")) {
+            $this->_helper->viewRenderer->setResponseSegment($this->getParam("_segment"));
+        }
+    }
 
     public static function dashesToCamelCase($string, $capitalizeFirstCharacter = true)
     {
